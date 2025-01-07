eb46e07227021c46a1ce95820b860e8e48f4e121
有限 <143406753+Whnneeww@users.noreply.github.com>
2025-01-07 23:20:17 +0900

    Update SNC.py

--- SNC.py
+++ SNC.py
@@ -1,23 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, request, Response
 import requests
 
 app = Flask(__name__)
+target_url = "https://sitedeta.i-i.f5.si"
 
-@app.route('/<path:requested_path>', methods=['GET'])
-def proxy(requested_path):
-    # sitedeta.i-i.f5.siへのURLを組み立てる
-    target_url = f'https://sitedeta.i-i.f5.si/{requested_path}'
-    
-    try:
-        # 指定したパスにリクエストを送信
-        response = requests.get(target_url)
-        
-        # ステータスコードと応答内容をそのまま返す
-        return (response.content, response.status_code, response.headers.items())
-    
-    except requests.exceptions.RequestException as e:
-        # エラーが発生した場合の応答
-        return jsonify({'error': str(e)}), 500
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def forward_request(path):
+    # 元のリクエストのメソッドとデータを保持する
+    method = request.method
+    data = request.get_data()
+    headers = {key: value for key, value in request.headers}
+
+    # target_urlにリクエストを送信
+    response = requests.request(method, f"{target_url}/{path}", data=data, headers=headers)
+
+    # 応答をそのまま返す
+    return Response(response.content, status=response.status_code, headers=dict(response.headers))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)

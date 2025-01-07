5d05441c66d2d1038029a36aa9cfb118eab2b2d7
有限 <143406753+Whnneeww@users.noreply.github.com>
2025-01-07 23:28:01 +0900

    Update SNC.py

--- SNC.py
+++ SNC.py
@@ -1,21 +1,20 @@
-from flask import Flask, request, Response
+from flask import Flask, Response
 import requests
 
 app = Flask(__name__)
-target_url = "https://sitedeta.i-i.f5.si"
 
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route('/<path:path>', methods=['GET'])
 def forward_request(path):
-    # 元のリクエストのメソッドとデータを保持する
-    method = request.method
-    data = request.get_data()
-    headers = {key: value for key, value in request.headers}
-
-    # target_urlにリクエストを送信
-    response = requests.request(method, f"{target_url}/{path}", data=data, headers=headers)
-
-    # 応答をそのまま返す
+    # sitedeta.i-i.f5.siへのリクエストを送信
+    response = requests.get(f"https://sitedeta.i-i.f5.si/{path}")
+    
+    # 元の応答をそのまま返す
     return Response(response.content, status=response.status_code, headers=dict(response.headers))
 
+@app.route('/', methods=['GET'])
+def root_request():
+    # /へのリクエストもsitedeta.i-i.f5.siにリダイレクトする
+    return forward_request('')  # 空のパスを渡すことでリクエストを転送
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)

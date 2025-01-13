77710ce2563d2146e1cbc0d855dc732b51dd15f5
有限 <143406753+Whnneeww@users.noreply.github.com>
2025-01-13 23:49:34 +0900

    Update SNC.py

--- SNC.py
+++ SNC.py
@@ -1,20 +1,26 @@
-import yt_dlp
-import sys
+from flask import Flask, request, send_file
+from pytube import YouTube
+import os
 
-def download_audio(url):
-    ydl_opts = {
-        'format': 'bestaudio/best',  # 最高音質の音声を選択
-        'extractaudio': True,         # 音声抽出を有効化
-        'audioformat': 'mp3',         # 音声形式をMP3に指定
-        'outtmpl': '%(title)s.%(ext)s',  # ファイル名を動画タイトルに
-    }
+app = Flask(__name__)
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])  # URLをリストで渡す
+@app.route('/')
+def index():
+    return '''
+        <form method="POST" action="/download">
+            YouTube URL: <input type="text" name="url" required>
+            <input type="submit" value="Download MP3">
+        </form>
+    '''
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("使用法: python youtube_mp3.py <YouTubeのURL>")
-    else:
-        video_url = sys.argv[1]  # コマンドライン引数からURLを取得
-        download_audio(video_url)
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    yt = YouTube(url)
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    output_file = audio_stream.download(filename='audio.mp3')
+    
+    return send_file(output_file, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)

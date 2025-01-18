7bd0880634ccfdf307ceacf34265e0e0dd5570f1
有限 <143406753+Whnneeww@users.noreply.github.com>
2025-01-18 23:24:19 +0900

    Update SNC.py

--- SNC.py
+++ SNC.py
@@ -1,15 +1,15 @@
-import http.server import socketserver # ホストとポート番号の設定 
+import http.server 
+import socketserver 
+# ホストとポート番号の設定 
 HOST = "0.0.0.0" 
-PORT = 2855 
-DIRECTORY = "storage"  
+PORT = 2855 DIRECTORY = "storage"  
 class CustomHandler(http.server.SimpleHTTPRequestHandler): 
-def translate_path(self, path):  
+def translate_path(self, path): 
   # リクエストによるパスを変更し、storageフォルダを公開する 
-  path = super().translate_path(path) 
-  return DIRECTORY + path  
-  # サーバーの設定 
+  path = super().translate_path(path)
+  return DIRECTORY + path  # サーバーの設定 
 handler = CustomHandler 
 with socketserver.TCPServer((HOST, PORT), handler) 
-as httpd:
-  print(f"サーバーが {HOST} のポート {PORT} で起動しました。")
+as httpd: 
+  print(f"サーバーが {HOST} のポート {PORT} で起動しました。") 
   httpd.serve_forever()

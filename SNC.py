7dce0830f7094dd9d0b7ea4894f89841e1d6069d
有限 <143406753+Whnneeww@users.noreply.github.com>
2025-01-18 11:47:54 +0900

    Update SNC.py

--- SNC.py
+++ SNC.py
@@ -1,26 +1,21 @@
-from flask import Flask, request, send_file
-from pytube import YouTube
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return '''
-        <form method="POST" action="/download">
-            YouTube URL: <input type="text" name="url" required>
-            <input type="submit" value="Download MP3">
-        </form>
-    '''
-
-@app.route('/download', methods=['POST'])
-def download():
-    url = request.form['url']
-    yt = YouTube(url)
-    audio_stream = yt.streams.filter(only_audio=True).first()
-    output_file = audio_stream.download(filename='audio.mp3')
-    
-    return send_file(output_file, as_attachment=True)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+from smbserver import SMBServer 
+from pyftpdlib.authorizers import DummyAuthorizer 
+from pyftpdlib.handlers import FTPHandler 
+from pyftpdlib.servers import FTPServer 
+import threading  
+storage_path = "./storage"  
+def start_smb_server(): smb_server = SMBServer() 
+smb_server.add_share('storage', storage_path) 
+smb_server.start('0.0.0.0', 2556) 
+print("SMBサーバーが起動しました。")  
+def start_ftp_server(): 
+authorizer = DummyAuthorizer() 
+authorizer.add_user("user", "12345", storage_path, perm="elradfmwMT") authorizer.add_anonymous(storage_path)  
+handler = FTPHandler handler.authorizer = authorizer  server = FTPServer(("0.0.0.0", 2557), handler) 
+server.serve_forever() print("FTPサーバーが起動しました。")  
+if __name__ == "__main__": smb_thread = threading.Thread(target=start_smb_server) 
+ftp_thread = threading.Thread(target=start_ftp_server)  
+smb_thread.start() 
+ftp_thread.start()  
+smb_thread.join() 
+ftp_thread.join() 

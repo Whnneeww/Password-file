de0e16283350299700f39e06e5230494ab8850b5
有限 <143406753+Whnneeww@users.noreply.github.com>
2025-01-18 12:32:08 +0900

    Update SNC.py

--- SNC.py
+++ SNC.py
@@ -1,27 +1,29 @@
-from smbserver import SMBServer 
-from pyftpdlib.authorizers 
-import DummyAuthorizer from pyftpdlib.handlers 
-import FTPHandler from pyftpdlib.servers 
-import FTPServer 
+from pysmb.smbserver import SMBServer 
+from pyftpdlib.authorizers import DummyAuthorizer 
+from pyftpdlib.handlers import FTPHandler 
+from pyftpdlib.servers import FTPServer 
 import threading  
-storage_path = "./storage" 
-def start_smb_server():
-  smb_server = SMBServer()  
-  smb_server.add_share('storage', storage_path)  
-  smb_server.start('0.0.0.0', 2556)  
+
+storage_path = "./storage"  
+
+def start_smb_server(): 
+  smb_server = SMBServer()
+  smb_server.add_share('storage', storage_path) 
+  smb_server.start('0.0.0.0', 2556) 
   print("SMBサーバーが起動しました。")  
   def start_ftp_server(): 
     authorizer = DummyAuthorizer() 
     authorizer.add_user("user", "12345", storage_path, perm="elradfmwMT") 
-    authorizer.add_anonymous(storage_path)   
-    handler = FTPHandler  
-    handler.authorizer = authorizer  
+    authorizer.add_anonymous(storage_path)  
+    handler = FTPHandler 
+    handler.authorizer = authorizer 
     server = FTPServer(("0.0.0.0", 2557), handler) 
-    server.serve_forever()  print("FTPサーバーが起動しました。")  
+    print("FTPサーバーが起動しました。") 
+    server.serve_forever()  
     if __name__ == "__main__": 
-      smb_thread = threading.Thread(target=start_smb_server)  
-      ftp_thread = threading.Thread(target=start_ftp_server)   
-      smb_thread.start()  
-      ftp_thread.start()   
-      smb_thread.join()  
+      smb_thread = threading.Thread(target=start_smb_server) 
+      ftp_thread = threading.Thread(target=start_ftp_server) 
+      smb_thread.start() 
+      ftp_thread.start()  
+      smb_thread.join() 
       ftp_thread.join()

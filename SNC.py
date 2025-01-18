8dfd62f44c278efd9145c91ec2f302c055f5f165
有限 <143406753+Whnneeww@users.noreply.github.com>
2025-01-18 23:19:46 +0900

    Update SNC.py

--- SNC.py
+++ SNC.py
@@ -1,29 +1,15 @@
-from pysmb.smbserver import SMBServer 
-from pyftpdlib.authorizers import DummyAuthorizer 
-from pyftpdlib.handlers import FTPHandler 
-from pyftpdlib.servers import FTPServer 
-import threading  
-
-storage_path = "./storage"  
-
-def start_smb_server(): 
-  smb_server = SMBServer()
-  smb_server.add_share('storage', storage_path) 
-  smb_server.start('0.0.0.0', 2556) 
-  print("SMBサーバーが起動しました。")  
-  def start_ftp_server(): 
-    authorizer = DummyAuthorizer() 
-    authorizer.add_user("user", "12345", storage_path, perm="elradfmwMT") 
-    authorizer.add_anonymous(storage_path)  
-    handler = FTPHandler 
-    handler.authorizer = authorizer 
-    server = FTPServer(("0.0.0.0", 2557), handler) 
-    print("FTPサーバーが起動しました。") 
-    server.serve_forever()  
-    if __name__ == "__main__": 
-      smb_thread = threading.Thread(target=start_smb_server) 
-      ftp_thread = threading.Thread(target=start_ftp_server) 
-      smb_thread.start() 
-      ftp_thread.start()  
-      smb_thread.join() 
-      ftp_thread.join()
+import http.server import socketserver # ホストとポート番号の設定 
+HOST = "0.0.0.0" 
+PORT = 2855 
+DIRECTORY = "storage"  
+class CustomHandler(http.server.SimpleHTTPRequestHandler): 
+def translate_path(self, path):  
+  # リクエストによるパスを変更し、storageフォルダを公開する 
+  path = super().translate_path(path) 
+  return DIRECTORY + path  
+  # サーバーの設定 
+handler = CustomHandler 
+with socketserver.TCPServer((HOST, PORT), handler) 
+as httpd:
+  print(f"サーバーが {HOST} のポート {PORT} で起動しました。")
+  httpd.serve_forever()

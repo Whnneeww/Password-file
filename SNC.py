f9202d19228a6862b5e82f52a97d0d9e4b807edc
有限 <143406753+Whnneeww@users.noreply.github.com>
2025-01-25 12:45:32 +0900

    Update SNC.py

--- SNC.py
+++ SNC.py
@@ -1,15 +1,14 @@
-import http.server 
-import socketserver 
-# ホストとポート番号の設定 
-HOST = "0.0.0.0" 
-PORT = 2855 DIRECTORY = "storage"  
-class CustomHandler(http.server.SimpleHTTPRequestHandler): 
-def translate_path(self, path): 
-  # リクエストによるパスを変更し、storageフォルダを公開する 
-  path = super().translate_path(path)
-  return DIRECTORY + path  # サーバーの設定 
-handler = CustomHandler 
-with socketserver.TCPServer((HOST, PORT), handler) 
-as httpd: 
-  print(f"サーバーが {HOST} のポート {PORT} で起動しました。") 
-  httpd.serve_forever()
+from PIL import Image 
+import random  
+
+width, height = 23040, 12960 
+image = Image.new("RGBA", (width, height))  # 画像の各ピクセルをランダムな色に設定 
+
+for y in range(height):
+for x in range(width):
+  r = random.randint(0, 255)
+  g = random.randint(0, 255)
+  b = random.randint(0, 255)
+  a = random.randint(0, 255)
+  image.putpixel((x, y), (r, g, b, a))  
+image.save("random_24K_image.png")
